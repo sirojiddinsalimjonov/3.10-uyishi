c1f8fbfe8b190eaffcadf9382477968fa3e87e21
sirojiddinsalimjonov <sirojiddinsalimjonovssz@gmail.com>
2022-05-26 17:28:39 +0500

    feedback

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ const orderEmail = document.querySelector('.user-email')
 const order = document.querySelector('.user-orders')
 const orderBtn = document.querySelector('.btn')
 const orderList = document.querySelector('.list')
-const checked = document.querySelector('.checkbox')
 
 function yourOrder(item) {
     item.preventDefault()
     const orderItem = document.createElement('li')
     orderItem.className = 'w-100 bg-success color-warning p-2 d-flex align-items-center text-warning  mb-3'
-    orderItem.textContent = 'yourName:' + orderName.value.trim() +' '+'yourEmail:'+ orderEmail.value.trim() +' '+'yourOrder:'+ order.valuee
+    orderItem.textContent = 'yourName:' + orderName.value.trim() +' '+'yourEmail:'+ orderEmail.value.trim() +' '+'yourOrder:'+ order.value
+    const box = document.createElement('input')
+    box.type = 'checkbox'
     orderName.value = ''
     orderEmail.value = ''
     order.value = ''
-    orderItem.append(checked)
+    orderItem.append(box)
     orderList.append(orderItem)
 }
 orderBtn.addEventListener('click', yourOrder)
